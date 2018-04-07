a45098de44ad3b7cfd14757543397f06634add9a
LittleAlex183 <gdo.alex@hotmail.com>
2018-04-07 01:57:03 -0500

    Pantallas de chuy

---
+++ BDProject/BDProject/Controllers/InventoriesController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BDProject.Models;
+
+namespace BDProject.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        private ProyectoEntities db = new ProyectoEntities();
+
+        // GET: Inventories
+        public ActionResult Index()
+        {
+            var inventories = db.Inventories.Include(i => i.ProductInformation).Include(i => i.Warehouse);
+            return View(inventories.ToList());
+        }
+
+        // GET: Inventories/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventories/Create
+        public ActionResult Create()
+        {
+            ViewBag.product_id = new SelectList(db.ProductInformations, "product_id", "product_name");
+            ViewBag.warehouse_id = new SelectList(db.Warehouses, "warehouse_id", "warehouse_spec");
+            return View();
+        }
+
+        // POST: Inventories/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "product_id,warehouse_id,quantity_on_hand")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Inventories.Add(inventory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.product_id = new SelectList(db.ProductInformations, "product_id", "product_name", inventory.product_id);
+            ViewBag.warehouse_id = new SelectList(db.Warehouses, "warehouse_id", "warehouse_spec", inventory.warehouse_id);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.product_id = new SelectList(db.ProductInformations, "product_id", "product_name", inventory.product_id);
+            ViewBag.warehouse_id = new SelectList(db.Warehouses, "warehouse_id", "warehouse_spec", inventory.warehouse_id);
+            return View(inventory);
+        }
+
+        // POST: Inventories/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "product_id,warehouse_id,quantity_on_hand")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventory).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.product_id = new SelectList(db.ProductInformations, "product_id", "product_name", inventory.product_id);
+            ViewBag.warehouse_id = new SelectList(db.Warehouses, "warehouse_id", "warehouse_spec", inventory.warehouse_id);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventories.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Inventory inventory = db.Inventories.Find(id);
+            db.Inventories.Remove(inventory);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BDProject/BDProject/Controllers/WarehousesController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BDProject.Models;
+
+namespace BDProject.Controllers
+{
+    public class WarehousesController : Controller
+    {
+        private ProyectoEntities db = new ProyectoEntities();
+
+        // GET: Warehouses
+        public ActionResult Index()
+        {
+            var warehouses = db.Warehouses.Include(w => w.Location);
+            return View(warehouses.ToList());
+        }
+
+        // GET: Warehouses/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Create
+        public ActionResult Create()
+        {
+            ViewBag.location_id = new SelectList(db.Locations, "location_id", "street_address");
+            return View();
+        }
+
+        // POST: Warehouses/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "warehouse_id,warehouse_spec,warehouse_name,location_id,wh_geo_location")] Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Warehouses.Add(warehouse);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.location_id = new SelectList(db.Locations, "location_id", "street_address", warehouse.location_id);
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.location_id = new SelectList(db.Locations, "location_id", "street_address", warehouse.location_id);
+            return View(warehouse);
+        }
+
+        // POST: Warehouses/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "warehouse_id,warehouse_spec,warehouse_name,location_id,wh_geo_location")] Warehouse warehouse)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(warehouse).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.location_id = new SelectList(db.Locations, "location_id", "street_address", warehouse.location_id);
+            return View(warehouse);
+        }
+
+        // GET: Warehouses/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Warehouse warehouse = db.Warehouses.Find(id);
+            if (warehouse == null)
+            {
+                return HttpNotFound();
+            }
+            return View(warehouse);
+        }
+
+        // POST: Warehouses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Warehouse warehouse = db.Warehouses.Find(id);
+            db.Warehouses.Remove(warehouse);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
