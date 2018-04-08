76b54d3e479c50aa3fd6cbea1bee263877595b42
AdriCabSan <32228937+AdriCabSan@users.noreply.github.com>
2018-04-08 11:08:48 -0500

    added styles in bundleconfig
    so you can use the new styles in the proyect

--- BDProject/BDProject/App_Start/BundleConfig.cs
+++ BDProject/BDProject/App_Start/BundleConfig.cs
@@ -25,7 +25,9 @@ namespace BDProject
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
 					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/site.css",
+                       "~/Content/Home.css",
+                       "~/Content/carousel.css"));
 		}
 	}
 }
